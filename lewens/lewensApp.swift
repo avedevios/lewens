8cfb3014bddb7e2eb7313bcbc9b251029f560398
averianovanton <antonaverianov@dgtl-factory.com>
2025-09-02 20:44:28 -0500

    Initial Commit

---
+++ lewens/lewensApp.swift
@@ -0,0 +1,17 @@
+//
+//  lewensApp.swift
+//  lewens
+//
+//  Created by Anton Averianov on 2025-09-02.
+//
+
+import SwiftUI
+
+@main
+struct lewensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
